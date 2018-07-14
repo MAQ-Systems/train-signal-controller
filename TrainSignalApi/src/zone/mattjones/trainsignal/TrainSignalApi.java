b5abdc6afe6fa7ae7bce9ca5f12d917e4112e33f
Matt Jones <matthew.d.jones1990@gmail.com>
2018-07-14 15:06:14 -0700

    Replace tabs with spaces in TrainSignalApi

--- TrainSignalApi/src/zone/mattjones/trainsignal/TrainSignalApi.java
+++ TrainSignalApi/src/zone/mattjones/trainsignal/TrainSignalApi.java
@@ -20,84 +20,84 @@ import zone.mattjones.trainsignal.TrainSignalMessage.LampState;
 import zone.mattjones.trainsignal.TrainSignalMessage.SignalColor;
 
 public class TrainSignalApi extends HttpServlet {
-	private static final long serialVersionUID = 06012014L;
+    private static final long serialVersionUID = 06012014L;
 
-	/** The port for the connection to the train signal to run on. */
+    /** The port for the connection to the train signal to run on. */
     private static final int SERVER_PORT = 19100;
-	
-	/** The thread handling connections to the train signal. */
-	private TrainSignalConnectionHandler mConnectionHandler;
-	
+    
+    /** The thread handling connections to the train signal. */
+    private TrainSignalConnectionHandler mConnectionHandler;
+    
     /**
      * Constructor that sets up the server socket listener for the arduino to connect to.
      */
     public TrainSignalApi() {
-    	mConnectionHandler = new TrainSignalConnectionHandler(SERVER_PORT);
+        mConnectionHandler = new TrainSignalConnectionHandler(SERVER_PORT);
     }
     
-	@Override
+    @Override
     public void init(ServletConfig config) {}
 
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		// Read input from the params in the URL.
-		String colorString = request.getParameter("color");
-		SignalColor color = SignalColor.RED;
-		if (colorString != null) {
-			switch(colorString.charAt(0)) {
-				case 'R':
-				case 'r':
-					color = SignalColor.RED;
-					break;
-				case 'Y':
-				case 'y':
-					color = SignalColor.YELLOW;
-					break;
-				case 'G':
-				case 'g':
-					color = SignalColor.GREEN;
-					break;
-			}
-		}
-		
-		String lampString = request.getParameter("lamp");
-		LampState lamp = LampState.OFF;
-		if (lampString != null) {
-			switch(lampString.charAt(0)) {
-				case '1':
-					lamp = LampState.ON;
-					break;
-				case '0':
-					lamp = LampState.OFF;
-					break;
-				case 'B':
-				case 'b':
-					lamp = LampState.BLINK;
-					break;
-			}
-		}
-		
-		String serverActionString = request.getParameter("serverAction");
-		if (serverActionString != null) {
-			switch(serverActionString.charAt(0)) {
-			case 'X':
-			case 'x':
-				mConnectionHandler.killServer();
-				break;
-			case 'R':
-			case 'r':
-				mConnectionHandler.resetServer();
-				break;
-			}
-		}
-		
-		byte[] signalMessage = TrainSignalMessage.generateMessage(color, lamp);
-		mConnectionHandler.addMessage(signalMessage);
-		
-		String messageString = new String(signalMessage);
-		response.getWriter().append("{\"currentState\":\"" + messageString + "\"}");
-		response.getWriter().flush();
-		response.getWriter().close();
-	}
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Read input from the params in the URL.
+        String colorString = request.getParameter("color");
+        SignalColor color = SignalColor.RED;
+        if (colorString != null) {
+            switch(colorString.charAt(0)) {
+                case 'R':
+                case 'r':
+                    color = SignalColor.RED;
+                    break;
+                case 'Y':
+                case 'y':
+                    color = SignalColor.YELLOW;
+                    break;
+                case 'G':
+                case 'g':
+                    color = SignalColor.GREEN;
+                    break;
+            }
+        }
+        
+        String lampString = request.getParameter("lamp");
+        LampState lamp = LampState.OFF;
+        if (lampString != null) {
+            switch(lampString.charAt(0)) {
+                case '1':
+                    lamp = LampState.ON;
+                    break;
+                case '0':
+                    lamp = LampState.OFF;
+                    break;
+                case 'B':
+                case 'b':
+                    lamp = LampState.BLINK;
+                    break;
+            }
+        }
+        
+        String serverActionString = request.getParameter("serverAction");
+        if (serverActionString != null) {
+            switch(serverActionString.charAt(0)) {
+            case 'X':
+            case 'x':
+                mConnectionHandler.killServer();
+                break;
+            case 'R':
+            case 'r':
+                mConnectionHandler.resetServer();
+                break;
+            }
+        }
+        
+        byte[] signalMessage = TrainSignalMessage.generateMessage(color, lamp);
+        mConnectionHandler.addMessage(signalMessage);
+        
+        String messageString = new String(signalMessage);
+        response.getWriter().append("{\"currentState\":\"" + messageString + "\"}");
+        response.getWriter().flush();
+        response.getWriter().close();
+    }
 }
--- TrainSignalApi/src/zone/mattjones/trainsignal/TrainSignalMessage.java
+++ TrainSignalApi/src/zone/mattjones/trainsignal/TrainSignalMessage.java
@@ -3,7 +3,7 @@
  * Author: Matt Jones
  * Date: 7.10.2018
  * Desc: Utilities for producing and parsing messages from the train signal controller (the
- * 		 arduino).
+ *          arduino).
  * 
  * Protocol:
  *       Since there are a grand total of 8 possible things the signal
@@ -25,73 +25,73 @@
 package zone.mattjones.trainsignal;
 
 public class TrainSignalMessage {
-	/** Possible colors that the signal can be. */
-	public enum SignalColor {
-		RED,
-		YELLOW,
-		GREEN
-	}
-	
-	/** States that the lamp can be in. */
-	public enum LampState {
-		ON,
-		BLINK,
-		OFF
-	}
-	
+    /** Possible colors that the signal can be. */
+    public enum SignalColor {
+        RED,
+        YELLOW,
+        GREEN
+    }
+    
+    /** States that the lamp can be in. */
+    public enum LampState {
+        ON,
+        BLINK,
+        OFF
+    }
+    
     // Different states for a signal to be in
-	private static final int SIGNAL_BASE    = 1;  // 00000001
-	private static final int SIGNAL_BLINK   = 2;  // 00000010
-	private static final int SIGNAL_RED     = 4;  // 00000100
-	private static final int SIGNAL_YELLOW  = 8;  // 00001000
-	private static final int SIGNAL_GREEN   = 16; // 00010000
-	private static final int SIGNAL_LAMP_ON = 32; // 00100000
-	private static final int SIGNAL_LAMP_OFF= 64; // 01000000
-	
-	/** The character all messages should end with. */
-	private static final char MESSAGE_TERMINATING_CHAR = '!';
-	
-	/** Private constructor to prevent instantiation. */
-	private TrainSignalMessage() {}
-	
-	/**
-	 * Generate a message to change the state of the train signal. This should be sent to the
-	 * arduino that's physically connected to the signal.
-	 * @param signalColor The color the signal should be.
-	 * @param lampState The state of the lamp (on/off/blink).
-	 * @return A message byte array.
-	 */
-	public static byte[] generateMessage(SignalColor signalColor, LampState lampState) {
-		byte signalState = SIGNAL_BASE;
-		
-		switch(signalColor) {
-			case RED:
-				signalState |= SIGNAL_RED;
-				break;
-			case YELLOW:
-				signalState |= SIGNAL_YELLOW;
-				break;
-			case GREEN:
-				signalState |= SIGNAL_GREEN;
-				break;
-		}
-		
-		switch(lampState) {
-			case ON:
-				signalState |= SIGNAL_LAMP_ON;
-				break;
-			case BLINK:
-				signalState |= SIGNAL_BLINK;
-				break;
-			case OFF:
-				signalState |= SIGNAL_LAMP_OFF;
-				break;
-		}
-		
-		byte[] message = new byte[2];
-		message[0] = signalState;
-		message[1] = MESSAGE_TERMINATING_CHAR;
-		
-		return message;
-	}
+    private static final int SIGNAL_BASE    = 1;  // 00000001
+    private static final int SIGNAL_BLINK   = 2;  // 00000010
+    private static final int SIGNAL_RED     = 4;  // 00000100
+    private static final int SIGNAL_YELLOW  = 8;  // 00001000
+    private static final int SIGNAL_GREEN   = 16; // 00010000
+    private static final int SIGNAL_LAMP_ON = 32; // 00100000
+    private static final int SIGNAL_LAMP_OFF= 64; // 01000000
+    
+    /** The character all messages should end with. */
+    private static final char MESSAGE_TERMINATING_CHAR = '!';
+    
+    /** Private constructor to prevent instantiation. */
+    private TrainSignalMessage() {}
+    
+    /**
+     * Generate a message to change the state of the train signal. This should be sent to the
+     * arduino that's physically connected to the signal.
+     * @param signalColor The color the signal should be.
+     * @param lampState The state of the lamp (on/off/blink).
+     * @return A message byte array.
+     */
+    public static byte[] generateMessage(SignalColor signalColor, LampState lampState) {
+        byte signalState = SIGNAL_BASE;
+        
+        switch(signalColor) {
+            case RED:
+                signalState |= SIGNAL_RED;
+                break;
+            case YELLOW:
+                signalState |= SIGNAL_YELLOW;
+                break;
+            case GREEN:
+                signalState |= SIGNAL_GREEN;
+                break;
+        }
+        
+        switch(lampState) {
+            case ON:
+                signalState |= SIGNAL_LAMP_ON;
+                break;
+            case BLINK:
+                signalState |= SIGNAL_BLINK;
+                break;
+            case OFF:
+                signalState |= SIGNAL_LAMP_OFF;
+                break;
+        }
+        
+        byte[] message = new byte[2];
+        message[0] = signalState;
+        message[1] = MESSAGE_TERMINATING_CHAR;
+        
+        return message;
+    }
 }
