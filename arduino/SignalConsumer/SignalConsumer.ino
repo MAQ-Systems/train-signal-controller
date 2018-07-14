9cb01e58100d40abded3fe23f40717a0d065bf25
Matt Jones <matthew.d.jones1990@gmail.com>
2018-07-13 17:29:42 -0700

    Fix signal consumer (arduino) software
    Notable changes:
    - Use simplified signal message that contains only signal state.
    - Remove initial message send (no longer required by server).
    - Update message reading loop to ignore empty messages (-1 means no data).

--- arduino/SignalConsumer/SignalConsumer.ino
+++ arduino/SignalConsumer/SignalConsumer.ino
@@ -22,26 +22,43 @@
 #define SIGNAL_LAMP_ON  32 // 00100000
 #define SIGNAL_LAMP_OFF 64 // 01000000
 
+#define MESSAGE_TERMINATING_CHAR '!'
+
+#define GREEN_PIN 13
+#define YELLOW_PIN 12
+#define RED_PIN 11
+
+typedef enum {
+  RED = 0,
+  YELLOW = 1,
+  GREEN = 2
+} SignalColor;
+
+typedef enum {
+  ON = 0,
+  BLINK = 1,
+  OFF = 2
+} LampState;
+
 /**
  * SignalMessage struct for parsing signal messages
  */
-typedef struct signalMessage {
-  char clientType;
-  boolean error;
-  char signalState;
+typedef struct {
+  LampState lampState;
+  SignalColor color;
 } SignalMessage;
 
 // network info
 byte mac[] = {0x90, 0xA2, 0xDA, 0x0F, 0x2C, 0x47};
 byte staticIp[] = {192, 168, 1, 150};
-byte serverIp[] = {24, 15, 183, 66};
+//byte serverIp[] = {24, 15, 183, 66};
+byte serverIp[] = {192, 168, 1, 105};
 char serverName[] = "www.iotitan.com";
 int serverPort = 19100;
 
 EthernetClient client;
 char readBuffer[BUFFER_SIZE];
 int readBufferPos;
-long prevTime;
 
 // prototypes
 boolean connectToServer();
@@ -67,10 +84,19 @@ void setup() {
   delay(1000);
   
   connectToServer();
+
+  if (client.connected()) {
+    Serial.println("Connected!");
+  } else {
+    Serial.println("Initial connection failed!");
+  }
   
   // if dhcp, request that it be maintained
   //Ethernet.maintain();
-  prevTime = millis();
+
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(YELLOW_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
 }
 
 /**
@@ -79,22 +105,7 @@ void setup() {
  */
 boolean connectToServer() {
   client.connect(serverIp, serverPort);
-  if(!client.connected()) {
-    return false; 
-  }
-  // read intro message
-  readBufferPos = 0;
-  while((readBuffer[readBufferPos] = client.read()) != '\0' && readBufferPos < BUFFER_SIZE) {
-    readBufferPos++;
-  }
-  readBuffer[readBufferPos] = '\0';
-  
-  printData(readBuffer, readBufferPos);
-  Serial.println();
-  
-  // notify server we intend to read
-  client.print("R|F|1\0");
-  
+  if(!client.connected()) return false;
   return true;
 }
 
@@ -102,40 +113,44 @@ boolean connectToServer() {
  * Main read/update loop
  */
 void loop() {
-  
-  long curTime = millis();
-  
   if(!client.connected()) {
     client.stop();
     if(!connectToServer()) {
-      Serial.println("waiting to reconnect");
+      Serial.println("Waiting to reconnect...");
       delay(10000); // if we can't connect, wait 10 sec and try again
     }
     else {
-      Serial.println("reconnected successfully!"); 
+      Serial.println("Reconnected!"); 
     }
   }
+
+  // Whether the network read was a signal message or empty.
+  bool readWasMessage = false;
   
   // wait for signal to switch for a bit before trying to read another (5 sec)
-  if(client.connected() && curTime - prevTime > 5000) {
-    prevTime = curTime;
-    
+  if(client.connected()) {
     readBufferPos = 0;
-    while((readBuffer[readBufferPos] = client.read()) != '\0' && readBufferPos < BUFFER_SIZE-1) {
-      Serial.write(readBuffer[readBufferPos]);
+    while((readBuffer[readBufferPos] = client.read()) != -1
+        && readBufferPos < BUFFER_SIZE-1
+        && readBuffer[readBufferPos] != MESSAGE_TERMINATING_CHAR) {
       readBufferPos++;
     }
-    readBuffer[readBufferPos] = '\0';
-    
-    printData(readBuffer, readBufferPos);
-    Serial.println();
+    readBuffer[readBufferPos] = MESSAGE_TERMINATING_CHAR;
     
     // parse the message and switch appropriate pins
     SignalMessage* sm = parseSignalMessage(readBuffer, readBufferPos);
     if(sm != NULL) {
+      readWasMessage = true;
       handleSignalMessage(sm);
       delete sm;
     }
+  }
+
+  // If a signal was received, wait 5 sec for it to change.
+  if (readWasMessage) {
+    delay(5000);
+  } else {
+    delay(100);
   }
 }
 
@@ -145,13 +160,25 @@ void loop() {
  * @param len The length of the message/array
  */
 void printData(char* msg, int len) {
+  Serial.write("\nlength: ");
+  Serial.write(len);
+  Serial.println();
   int i;
   for(i = 0; i < len; i++) {
-    if(msg[i] == '\0') {
+    if(msg[i] == MESSAGE_TERMINATING_CHAR) {
       break;
     }
     Serial.write(msg[i]);
   }
+  Serial.println();
+  for(i = 0; i < len; i++) {
+    if(msg[i] == MESSAGE_TERMINATING_CHAR) {
+      break;
+    }
+    Serial.write((int)msg[i]);
+    Serial.write(", ");
+  }
+  Serial.println();
 }
 
 /**
@@ -160,39 +187,32 @@ void printData(char* msg, int len) {
  */
 void handleSignalMessage(void* smp) {
   SignalMessage* sm = (SignalMessage*)smp;
-  int state = (int)sm->signalState;
-  
-  // invalid code?
-  if(state & SIGNAL_BASE == 0) {
-    return;
+
+  digitalWrite(RED_PIN, LOW);
+  digitalWrite(YELLOW_PIN, LOW);
+  digitalWrite(GREEN_PIN, LOW);
+
+  Serial.write("Handling message: ");
+  if (sm->color == RED) {
+    digitalWrite(RED_PIN, HIGH);
+    Serial.write("RED");
+  } else if (sm->color == YELLOW) {
+    digitalWrite(YELLOW_PIN, HIGH);
+    Serial.write("YELLOW");
+  } else if (sm->color == GREEN) {
+    digitalWrite(GREEN_PIN, HIGH);
+    Serial.write("GREEN");
+  }
+
+  if (sm->lampState == ON) {
+    Serial.write(" ON");
+  } else if (sm->lampState == BLINK) {
+    Serial.write(" BLINK");
+  } else if (sm->lampState == OFF) {
+    Serial.write(" OFF");
   }
   
-  Serial.println("\n\n");
-  
-  // blink?
-  if(state & SIGNAL_BLINK > 0) {
-    Serial.println("Blinking");
-  }
-  
-  // signal color
-  if(state & SIGNAL_RED > 0) {
-    Serial.println("Red");
-  }
-  else if(state & SIGNAL_YELLOW > 0) {
-    Serial.println("Yellow");
-  }
-  else if(state & SIGNAL_GREEN > 0) {
-    Serial.println("Green");
-  }
-  
-  // lamp on or off
-  if(state & SIGNAL_LAMP_ON > 0) {
-    Serial.println("Turning lamp on...");
-  }
-  
-  if(state & SIGNAL_LAMP_OFF > 0) {
-    Serial.println("Turning lamp off...");
-  }
+  Serial.println("\n");
 }
 
 /**
@@ -202,19 +222,16 @@ void handleSignalMessage(void* smp) {
  * @param len The length of the message
  */
 boolean isValidMessage(char* msg, int len) {
-  if(msg == NULL) {
-    return false;
-  }
+  if(msg == NULL || len <= 0) return false;
+
+  // invalid code?
+  if((msg[0] & SIGNAL_BASE) == 0) return false;
 
   // make sure message is not too large
-  if(len > 32) {
-    return false;
-  }
+  if(len > 32) return false;
 
   // make sure message closing character is present
-  if(msg[len-1] == '\0') {
-    return false;
-  }
+  if(msg[len-1] == MESSAGE_TERMINATING_CHAR) return false;
 
   return true;
 }
@@ -226,42 +243,22 @@ boolean isValidMessage(char* msg, int len) {
  * @return Pointer to a new SignalMessage
  */
 SignalMessage* parseSignalMessage(char* msg, int len) {
-  if(!isValidMessage(msg, len)) {
-    return NULL;
-  }
+  if(!isValidMessage(msg, len)) return NULL;
 
   SignalMessage* sm = new SignalMessage;
 
   // init signal message
-  sm->clientType = 'X';
-  sm->error = false;
-  sm->signalState = 1;
+  sm->lampState = OFF;
+  sm->color = RED;
 
-  char *tokSave;
-  char *token = strtok_r(msg, "|", &tokSave);
-  int elementNo = 0;
-
-  // fill struct by stepping through tokens (easy to keep default settings)
-  while(token != NULL) {
-    if(strlen(token) > 0) {
-      switch(elementNo) {
-        case 0:
-          sm->clientType = token[0];
-          break;
-
-        case 1:
-          sm->error = token[0]=='T'?true:false;
-          break;
-
-        case 2:
-          sm->signalState = token[0];
-          break;
-      }
-    }
-    elementNo++;
-    token = strtok_r(NULL, "|", &tokSave);
+  if ((msg[0] & SIGNAL_RED) > 0) {
+    sm->color = RED;
+  } else if ((msg[0] & SIGNAL_YELLOW) > 0) {
+    sm->color = YELLOW;
+  } else if ((msg[0] & SIGNAL_GREEN) > 0) {
+    sm->color = GREEN;
   }
-
+  
   return sm;
 }
 
